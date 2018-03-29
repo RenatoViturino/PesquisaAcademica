850cba0cac6020f23ccbcfee62412be6dadc8acb
rosl <rosl@PCALP054>
2018-03-29 08:44:47 -0300

    mudanças

--- Pesquisa_com_Watson/Formularios/frmPrincipal.Designer.cs
+++ Pesquisa_com_Watson/Formularios/frmPrincipal.Designer.cs
@@ -31,14 +31,20 @@
             this.label1 = new System.Windows.Forms.Label();
             this.txtPesquisa = new System.Windows.Forms.TextBox();
             this.btnPesquisar = new System.Windows.Forms.Button();
-            this.gkWeb = new Gecko.GeckoWebBrowser();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.chkPt = new System.Windows.Forms.CheckBox();
             this.chkIng = new System.Windows.Forms.CheckBox();
             this.chkEsp = new System.Windows.Forms.CheckBox();
             this.chkFra = new System.Windows.Forms.CheckBox();
             this.chkIta = new System.Windows.Forms.CheckBox();
+            this.tbIdiomas = new System.Windows.Forms.TabControl();
+            this.tbPgPort = new System.Windows.Forms.TabPage();
+            this.tbPgIng = new System.Windows.Forms.TabPage();
+            this.tbPgEsp = new System.Windows.Forms.TabPage();
+            this.tbPgFra = new System.Windows.Forms.TabPage();
+            this.tbPgIta = new System.Windows.Forms.TabPage();
             this.groupBox1.SuspendLayout();
+            this.tbIdiomas.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -60,22 +66,13 @@
             // 
             // btnPesquisar
             // 
-            this.btnPesquisar.Location = new System.Drawing.Point(873, 13);
+            this.btnPesquisar.Location = new System.Drawing.Point(840, 19);
             this.btnPesquisar.Name = "btnPesquisar";
-            this.btnPesquisar.Size = new System.Drawing.Size(141, 32);
+            this.btnPesquisar.Size = new System.Drawing.Size(141, 26);
             this.btnPesquisar.TabIndex = 2;
             this.btnPesquisar.Text = "Pesquisar Assunto";
             this.btnPesquisar.UseVisualStyleBackColor = true;
             this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
-            // 
-            // gkWeb
-            // 
-            this.gkWeb.FrameEventsPropagateToMainWindow = false;
-            this.gkWeb.Location = new System.Drawing.Point(137, 147);
-            this.gkWeb.Name = "gkWeb";
-            this.gkWeb.Size = new System.Drawing.Size(877, 533);
-            this.gkWeb.TabIndex = 3;
-            this.gkWeb.UseHttpActivityObserver = false;
             // 
             // groupBox1
             // 
@@ -86,7 +83,7 @@
             this.groupBox1.Controls.Add(this.chkPt);
             this.groupBox1.Location = new System.Drawing.Point(137, 51);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(877, 90);
+            this.groupBox1.Size = new System.Drawing.Size(844, 90);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             // 
@@ -140,13 +137,74 @@
             this.chkIta.Text = "Italiano";
             this.chkIta.UseVisualStyleBackColor = true;
             // 
+            // tbIdiomas
+            // 
+            this.tbIdiomas.Controls.Add(this.tbPgPort);
+            this.tbIdiomas.Controls.Add(this.tbPgIng);
+            this.tbIdiomas.Controls.Add(this.tbPgEsp);
+            this.tbIdiomas.Controls.Add(this.tbPgFra);
+            this.tbIdiomas.Controls.Add(this.tbPgIta);
+            this.tbIdiomas.Location = new System.Drawing.Point(21, 147);
+            this.tbIdiomas.Name = "tbIdiomas";
+            this.tbIdiomas.SelectedIndex = 0;
+            this.tbIdiomas.Size = new System.Drawing.Size(960, 533);
+            this.tbIdiomas.TabIndex = 5;
+            // 
+            // tbPgPort
+            // 
+            this.tbPgPort.Location = new System.Drawing.Point(4, 29);
+            this.tbPgPort.Name = "tbPgPort";
+            this.tbPgPort.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgPort.Size = new System.Drawing.Size(952, 500);
+            this.tbPgPort.TabIndex = 0;
+            this.tbPgPort.Text = "Português";
+            this.tbPgPort.UseVisualStyleBackColor = true;
+            // 
+            // tbPgIng
+            // 
+            this.tbPgIng.Location = new System.Drawing.Point(4, 29);
+            this.tbPgIng.Name = "tbPgIng";
+            this.tbPgIng.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgIng.Size = new System.Drawing.Size(985, 500);
+            this.tbPgIng.TabIndex = 1;
+            this.tbPgIng.Text = "Inglês";
+            this.tbPgIng.UseVisualStyleBackColor = true;
+            // 
+            // tbPgEsp
+            // 
+            this.tbPgEsp.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tbPgEsp.Location = new System.Drawing.Point(4, 29);
+            this.tbPgEsp.Name = "tbPgEsp";
+            this.tbPgEsp.Size = new System.Drawing.Size(985, 500);
+            this.tbPgEsp.TabIndex = 2;
+            this.tbPgEsp.Text = "Espanhol";
+            this.tbPgEsp.UseVisualStyleBackColor = true;
+            // 
+            // tbPgFra
+            // 
+            this.tbPgFra.Location = new System.Drawing.Point(4, 29);
+            this.tbPgFra.Name = "tbPgFra";
+            this.tbPgFra.Size = new System.Drawing.Size(985, 500);
+            this.tbPgFra.TabIndex = 3;
+            this.tbPgFra.Text = "Francês";
+            this.tbPgFra.UseVisualStyleBackColor = true;
+            // 
+            // tbPgIta
+            // 
+            this.tbPgIta.Location = new System.Drawing.Point(4, 29);
+            this.tbPgIta.Name = "tbPgIta";
+            this.tbPgIta.Size = new System.Drawing.Size(985, 500);
+            this.tbPgIta.TabIndex = 4;
+            this.tbPgIta.Text = "Italiano";
+            this.tbPgIta.UseVisualStyleBackColor = true;
+            // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 692);
+            this.Controls.Add(this.tbIdiomas);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.gkWeb);
             this.Controls.Add(this.btnPesquisar);
             this.Controls.Add(this.txtPesquisa);
             this.Controls.Add(this.label1);
@@ -156,6 +214,7 @@
             this.Text = "Pesquisa Com Watson";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.tbIdiomas.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -166,13 +225,18 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtPesquisa;
         private System.Windows.Forms.Button btnPesquisar;
-        private Gecko.GeckoWebBrowser gkWeb;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.CheckBox chkIta;
         private System.Windows.Forms.CheckBox chkFra;
         private System.Windows.Forms.CheckBox chkEsp;
         private System.Windows.Forms.CheckBox chkIng;
         private System.Windows.Forms.CheckBox chkPt;
+        private System.Windows.Forms.TabControl tbIdiomas;
+        private System.Windows.Forms.TabPage tbPgPort;
+        private System.Windows.Forms.TabPage tbPgIng;
+        private System.Windows.Forms.TabPage tbPgEsp;
+        private System.Windows.Forms.TabPage tbPgFra;
+        private System.Windows.Forms.TabPage tbPgIta;
     }
 }
 
--- Pesquisa_com_Watson/Program.cs
+++ Pesquisa_com_Watson/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Pesquisa_com_Watson
@@ -16,9 +11,7 @@ namespace Pesquisa_com_Watson
         [STAThread]
         static void Main()
         {
-            var programDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            Gecko.Xpcom.Initialize("xulrunner/");
-           // Gecko.XpCom.Initialize(Path.Combine(programDirectory, "xulrunner"));
+           
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmPrincipal());
